6ec13e221315f9179348fc5c915880aeb8ed4504
parksunwoo <psw261@gmail.com>
2023-03-02 00:51:58 +0900

    feat: celery-task
    - Automatically Retrying Failed Celery Tasks
    
    ref. https://testdriven.io/blog/retrying-failed-celery-tasks/

---
+++ django-celery-project/tdd/forms.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.forms import UserCreationForm
+
+from tdd.models import Member
+
+
+class MemberForm(UserCreationForm):
+
+    class Meta:
+        model = Member
+        fields = ('username', 'email', 'password1', 'password2', 'avatar')
+        
